98414a02304c7bbc592e3e5a144ba36f34bd8895
Clint.Network <me@clint.network>
2021-05-16 18:44:25 +0300

    Create api-docs page

---
+++ pages/api-docs.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="text-center px-3 text-break">
+		<h1 class="display-4">
+			API <strong class="fw-bold">Documentation</strong> 🚩
+		</h1>
+		<p class="lead">Huh, an API? Yes sure, but for what I don't yet know :D</p>
+		<p>(But if you have any idea, join on <a href="https://t.me/clintnetwork" target="_blank">me</a> on telegram)</p>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "api-docs"
+}
+</script>
+
+<style scoped>
+
+</style>
