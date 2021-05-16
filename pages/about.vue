164bd1490c13a2d4f3a5cf2236265cccd6dfc5d1
Clint.Network <me@clint.network>
2021-05-16 18:44:20 +0300

    Add CTA buttons for about page

--- pages/about.vue
+++ pages/about.vue
@@ -6,10 +6,20 @@
 		</h1>
 		<p class="lead">Hey, I'm Clint! Solana.sh is born during the night of 15 May 2021.</p>
 		<p>What is it? Hmm little bit hard to said, for now just a price ticket, but it will evolve!</p>
-		<p class="lead mt-5">
+		<p class="lead my-5">
 			If you want to send me a tips:<br /> <a href="https://explorer.solana.com/address/7X1ex1EaW9GM5Fxt5X7qgCLxSePa2piu3NBq2GiM7yuL" target="_blank" class="text-inherit fw-bold">7X1ex1EaW9GM5Fxt5X7qgCLxSePa2piu3NBq2GiM7yuL</a>
 			<sup><i class="bx bx-link-external"></i></sup>
 		</p>
+		<div class="d-flex buttons justify-content-center">
+			<a href="https://t.me/clintnetwork " target="_blank" class="btn btn-lg d-flex align-items-center btn-primary rounded-pill text-white px-4 mt-4">
+				<i class="bx bxl-telegram me-2"></i>
+				My Telegram
+			</a>
+			<a href="https://github.com/clintnetwork" target="_blank" class="btn btn-lg d-flex align-items-center btn-secondary rounded-pill px-4 ms-lg-3 mt-4">
+				<i class="bx bxl-github me-2"></i>
+				My GitHub
+			</a>
+		</div>
 	</div>
 </template>
 
