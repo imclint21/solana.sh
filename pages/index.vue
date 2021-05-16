6d162289c72eacb387977a062268e5ada54431f6
Clint.Network <me@clint.network>
2021-05-16 19:52:34 +0300

    Change secondary CTA

--- pages/index.vue
+++ pages/index.vue
@@ -15,12 +15,12 @@
 			  <i class="bx bxs-coin-stack me-2"></i>
 			  Buy Solana ($SOL)
 		  </a>
-		  <button class="btn btn-lg d-flex align-items-center btn-secondary rounded-pill px-4 ms-lg-3 mt-4">
-<!--			  <i class="bx bxs-binoculars me-2"></i>-->
-<!--			  About Us-->
-			  <i class="bx bxs-book-content me-2"></i>
-			  API Documentation
-		  </button>
+		  <NuxtLink to="/about" class="btn btn-lg d-flex align-items-center btn-secondary rounded-pill px-4 ms-lg-3 mt-4">
+			  <i class="bx bxs-binoculars me-2"></i>
+			  About Solana.sh
+<!--			  <i class="bx bxs-book-content me-2"></i>-->
+<!--			  API Documentation-->
+		  </NuxtLink>
 	  </div>
   </div>
 </template>
