6e5ca3a24914a45ab8f11e6bc2f145645dc9b8c0
Clint.Network <me@clint.network>
2021-05-16 11:54:37 +0300

    Create footer component

---
+++ components/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+	<footer class="footer mt-auto py-4">
+		<div class="container small">
+			<div class="row">
+				<div class="col d-flex text-center justify-content-center align-items-center">
+					<p class="mb-0">Copyright &copy; {{ new Date().getFullYear() }} Solana.sh - Crafted with 💜 by Clint.network -
+						<i class="bx bxl-github ms-1"></i>
+						Fork me on <a class="text-inherit" href="https://github.com/chianetspace/chianetspace" target="_blank">GitHub</a>
+					</p>
+				</div>
+			</div>
+		</div>
+	</footer>
+</template>
+
+<script>
+export default {
+	name: "Footer"
+}
+</script>
+
+<style scoped>
+
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -9,18 +9,7 @@
 		<main role="main" class="d-flex justify-content-center align-items-center h-100 pb-5">
 			<Nuxt />
 		</main>
-		<footer class="footer mt-auto py-4">
-			<div class="container small">
-				<div class="row">
-					<div class="col d-flex text-center justify-content-center align-items-center">
-						<p class="mb-0">Copyright &copy; {{ new Date().getFullYear() }} Solana.sh - Crafted with 💜 by Clint.network -
-							<i class="bx bxl-github ms-1"></i>
-							Fork me on <a class="text-inherit" href="https://github.com/chianetspace/chianetspace" target="_blank">GitHub</a>
-						</p>
-					</div>
-				</div>
-			</div>
-		</footer>
+		<Footer />
 	</div>
 </template>
 
