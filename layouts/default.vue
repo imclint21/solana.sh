a5e829b32bc98982a81922df53485d1f78552fbe
Clint.Network <me@clint.network>
2021-05-16 11:46:53 +0300

    Add hotjar

--- layouts/default.vue
+++ layouts/default.vue
@@ -24,6 +24,26 @@
 	</div>
 </template>
 
+<script>
+export default {
+	created() {
+		if(process.client) {
+			(function (h, o, t, j, a, r) {
+				h.hj = h.hj || function () {
+					(h.hj.q = h.hj.q || []).push(arguments)
+				};
+				h._hjSettings = {hjid: 2402974, hjsv: 6};
+				a = o.getElementsByTagName('head')[0];
+				r = o.createElement('script');
+				r.async = 1;
+				r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+				a.appendChild(r);
+			})(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
+		}
+	}
+}
+</script>
+
 <style>
 #app
 {
